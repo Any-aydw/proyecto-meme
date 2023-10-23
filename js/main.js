d9c01a03d9957e7a22a3ec0e4958f58a33c82335
Anabela Pandolfo <anabelasoledad@gmail.com>
2023-10-22 21:08:57 -0300

    input number, input range

--- js/main.js
+++ js/main.js
@@ -20,6 +20,10 @@ $("#text").addEventListener("input", (e)=>{
     $(".meme-image").style.backgroundImage=`url(${e.target.value})`
 })
 
+$("#color-text").addEventListener("input", (e) => {
+    $(".meme-image").style.backgroundColor= e.target.value
+})
+
 $("#color").addEventListener("input", (e)=>{
     $(".meme-text1").style.backgroundColor= e.target.value
 })
@@ -40,21 +44,21 @@ $("#font").addEventListener("input", (e)=>{
     $(".bottom-text").style.fontFamily= e.target.value
 })
 
-$("#number").addEventListener("input", (e) => {
+$("#number").addEventListener("input", (e) =>{
     $(".top-text") .style .fontSize = `${e.target.value}px`
 })
-const allFiltersMeme = () => {
-    $(".meme-image").style.filter = `brightness (${$("#brightness").value}) opacity(${$("#opacity").value}) contrast(${$("#contraste").value}%) blur(${$("#blur").value}px) grayscale (${$("#grayscale").value}%) sepia (${$("#sepia").value}%) hue-rotation (${$("#hue-rotation").value} deg ) saturation (${$("#saturation").value}%) invert (${$("#invert").value})`
+const allFiltersMeme =()=>{
+    $(".meme-image").style.filter=`brightness(${$("#brightness").value})opacity(${$("#opacity").value})contrast(${$("#contraste").value}%)blur(${$("#blur").value}px)grayscale(${$("#grayscale").value}%)sepia(${$("#sepia").value}%) hue-rotation(${$("#hue-rotation").value}deg)saturation(${$("#saturation").value}%)invert(${$("#invert").value})`
 }
-$("#opacity").addEventListener ("input", allFiltersMeme)
-$("#brightness").addEventListener ("input", allFiltersMeme)
-$("#contrast").addEventListener ("input", allFiltersMeme)
-$("#blur").addEventListener ("input", allFiltersMeme)
-$("#grayscale").addEventListener ("input", allFiltersMeme)
-$("#sepia").addEventListener ("input", allFiltersMeme)
-$("#hue-rotation").addEventListener ("input", allFiltersMeme)
-$("#saturation").addEventListener ("input", allFiltersMeme)
-$("#invert").addEventListener ("input", allFiltersMeme)
+$("#opacity").addEventListener("input",allFiltersMeme)
+$("#brightness").addEventListener("input",allFiltersMeme)
+$("#contrast").addEventListener("input",allFiltersMeme)
+$("#blur").addEventListener("input",allFiltersMeme)
+$("#grayscale").addEventListener("input",allFiltersMeme)
+$("#sepia").addEventListener("input", allFiltersMeme)
+$("#hue-rotation").addEventListener("input",allFiltersMeme)
+$("#saturation").addEventListener("input",allFiltersMeme)
+$("#invert").addEventListener("input",allFiltersMeme)
 
 // evento de boton reset no funciona ver
 // $("#reset").addEventListener ("click", () =>{
