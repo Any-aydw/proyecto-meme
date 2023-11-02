6bb95172324bb7323b3b55d9215f644227112147
Anabela Pandolfo <anabelasoledad@gmail.com>
2023-11-02 14:24:22 -0300

    optimizacion de codigo

--- js/main.js
+++ js/main.js
@@ -27,30 +27,20 @@ $("#color-text").addEventListener("input", (e) => {
 
 $("#color").addEventListener("input", (e)=>{
     $(".meme-text1").style.backgroundColor= e.target.value
-})
-
-$("#color").addEventListener("input", (e)=>{
     $(".meme-text2").style.backgroundColor= e.target.value
 })
-
 $("#color-font") .addEventListener("input", (e) => {
     $(".div-meme") .style.color= e.target.value
 })
 
 $("#font").addEventListener("input", (e)=>{
     $(".top-text").style.fontFamily= e.target.value
-})
-$("#font").addEventListener("input", (e)=>{
     $(".bottom-text").style.fontFamily= e.target.value
 })
-
 $("#font-number").addEventListener("input", (e) =>{
     $(".top-text") .style .fontSize = `${e.target.value}px`
-})
-$("#font-number").addEventListener("input", (e) =>{
     $(".bottom-text") .style .fontSize = `${e.target.value}px`
 })
-
 $("#displayTopText").addEventListener("input", (e) =>{
     if (e.target.checked){
         $(".meme-text1").style.display = "none"
@@ -65,7 +55,6 @@ $("#displayBottomText").addEventListener("input", (e) => {
         $(".meme-text2").style.display = "block"
     }
 })
-
 $("#fondoTrans").addEventListener ("input", (e) =>{
    if (e.target.checked){
     $(".meme-text1").style.backgroundColor="transparent"
@@ -84,36 +73,24 @@ $("#fondoTrans").addEventListener ("input", (e) =>{
 
 $("#text-line").addEventListener ("input", (e) => {
     $(".meme-text1").style.lineHeight= e.target.value
+    $(".meme-text2").style.lineHeight= e.target.value
 })
- $("#text-line").addEventListener ("input", (e) => {
-   $(".meme-text2").style.lineHeight= e.target.value
- })
 
 $("#text-padding").addEventListener ("input", (e) => {
     $(".meme-text1").style.padding = `${e.target.value}px`
+    $(".meme-text2").style.padding = `${e.target.value}px`
 })
- $("#text-padding").addEventListener ("input", (e) => {
-  $(".meme-text2").style.padding = `${e.target.value}px`
- })
-
 $(".btn-claro").addEventListener ("click", () => {
     $(".meme-text1").style.webkitTextStroke= "white 1px"
-})
-$(".btn-claro").addEventListener ("click", () => {
     $(".meme-text2").style.webkitTextStroke= "white 1px"
- })
-
-$(".btn-oscuro").addEventListener ("click", (e) => {
- $(".meme-text1").style.webkitTextStroke= "black 1px"
- })
-$(".btn-oscuro").addEventListener ("click", (e) => {
-   $(".meme-text2").style.webkitTextStroke= "black 1px"
 })
+$(".btn-oscuro").addEventListener ("click", (e) => {
+    $(".meme-text1").style.webkitTextStroke= "black 1px"
+    $(".meme-text2").style.webkitTextStroke= "black 1px"
+ })
 $(".btn-ninguno").addEventListener ("click", (e) => {
-     $(".meme-text1").style.webkitTextStroke= "none"
-})
- $(".btn-ninguno").addEventListener ("click", (e) => {
-     $(".meme-text2").style.webkitTextStroke= "none"
+    $(".meme-text1").style.webkitTextStroke= "none"
+    $(".meme-text2").style.webkitTextStroke= "none"
 })
 
 $("#color-img").addEventListener ("input", (e) => {
@@ -121,25 +98,16 @@ $("#color-img").addEventListener ("input", (e) => {
 })
 $("#align-left").addEventListener ("click", (e) => {
     $(".top-text").style.textAlign="left"
- })
- $("#align-left").addEventListener ("click", (e) => {
     $(".bottom-text").style.textAlign="left"
  })
-
 $("#align-right").addEventListener ("click", (e) => {
- $(".top-text").style.textAlign="right"
+   $(".top-text").style.textAlign="right"
+   $(".bottom-text").style.textAlign="right"
 })
-$("#align-right").addEventListener ("click", (e) => {
-    $(".bottom-text").style.textAlign="right"
-   })
-
 $("#align-center").addEventListener ("click", (e) => {
  $(".top-text").style.textAlign="center"
+ $(" .bottom-text").style.textAlign="center"
 })
-$("#align-center").addEventListener ("click", (e) => {
-    $(" .bottom-text").style.textAlign="center"
-   })
-
 $(".button-panel-img").addEventListener ("click", (e) => {
     if (e.target.click){
       $("#meme-panel-image").classList.remove("visibility--hidden")
